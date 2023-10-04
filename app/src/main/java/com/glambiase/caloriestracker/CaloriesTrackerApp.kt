09abf15cc2a825db537b2b32b1a96467404c2307
Giampaolo Lambiase <giampaololambiase@gmail.com>
2023-10-04 16:27:57 +0200

    dagger_hilt setup

---
+++ app/src/main/java/com/glambiase/caloriestracker/CaloriesTrackerApp.kt
@@ -0,0 +1,7 @@
+package com.glambiase.caloriestracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CaloriesTrackerApp : Application()
---
+++ app/src/main/java/com/glambiase/caloriestracker/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.glambiase.caloriestracker.di
+
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import com.glambiase.core.data.preferences.DefaultPreferences
+import com.glambiase.core.domain.preferences.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(app: Application): SharedPreferences =
+        app.getSharedPreferences("shared_preferences", MODE_PRIVATE)
+
+    @Provides
+    @Singleton
+    fun providePreferences(sharedPreferences: SharedPreferences): Preferences =
+        DefaultPreferences(sharedPreferences)
+}
