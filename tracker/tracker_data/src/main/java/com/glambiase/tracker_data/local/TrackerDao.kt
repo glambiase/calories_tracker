516761bea3f715ec8706b12cea9537ffb34e7603
Giampaolo Lambiase <giampaololambiase@gmail.com>
2023-12-14 18:08:26 +0100

    room setup

---
+++ tracker/tracker_data/src/main/java/com/glambiase/tracker_data/local/TrackerDao.kt
@@ -0,0 +1,26 @@
+package com.glambiase.tracker_data.local
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.glambiase.tracker_data.local.entity.TrackedFoodEntity
+import kotlinx.coroutines.flow.Flow
+
+interface TrackerDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTrackedFood(trackedFoodEntity: TrackedFoodEntity)
+
+    @Delete
+    suspend fun deleteTrackedFood(trackedFoodEntity: TrackedFoodEntity)
+
+    @Query(
+        """
+            SELECT * 
+            FROM TrackedFoodEntity
+            WHERE dayOfMonth = :day AND month = :month AND year = :year
+        """
+    )
+    suspend fun getFoodsForDate(day: Int, month: Int, year: Int): Flow<List<TrackedFoodEntity>>
+}
